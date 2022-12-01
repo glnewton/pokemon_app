be1638d9768fd93d940df72540c8ea183a02823f
Gary N <gary.newton@ymail.com>
2022-12-01 16:11:30 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -20,11 +20,12 @@ app.get('/pokemon', function(req, res){
     res.render('Index', { pokemon: pokemon });
 });   
 
-// app.get('/pokemon/:indexOfPokemonArray', function(req, res){
+app.get('/pokemon/:id', function(req, res){
+    res.send(req.params.id);
 //     res.render('Show', { //second param must be an object
 //         pokemon: fruits[req.params.indexOfPokemonArray] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfPokemonArray]
 //     });
-// });  
+});  
 
 // app.get('/pokemon', function(req, res){
 //     res.render('Index', {pokemon: pokemon});
