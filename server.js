a26eab3d34b1c58de588b0963795711f94987f9a
Gary N <gary.newton@ymail.com>
2022-12-06 10:52:11 -0500

    updated added env info for Render test deployment.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 const pokemon = require('./models/pokemon.js')
 //const Index =require('./views/Index.jsx')
 
