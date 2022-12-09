679311af76e6efd9d69cc748bceb57989e8727bf
Gary N <gary.newton@ymail.com>
2022-12-09 12:12:46 -0500

    v2 assignment

--- server.js
+++ server.js
@@ -1,30 +1,78 @@
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const pokemon = require('./models/pokemon.js')
-//const Index =require('./views/Index.jsx')
+const PORT = process.env.PORT || 3000;const mongoose = require('mongoose');
+const Pokemon = require('./models/pokemon');
+
+// Set up Middleware
+
+app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+
+// Setting up Mongoose
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.once('open', ()=> {
+    console.log('Connected to MongoDB')
+})
+mongoose.set('strictQuery', true);
+
+const db = mongoose.connection;
+
+// Root Route -- WORKS
 
 app.get('/', function(req, res){
     res.send('Welcome to the Pokemon App!');
 });
 
-// app.get('/pokemon', function(req, res){
-//     res.send(pokemon);
-// }); 
+// Index route = Show all records -- WORKS
 
-app.get('/pokemon', function(req, res){
-    res.render('Index', { pokemon: pokemon });
-});   
+app.get('/pokemon', (req, res) => {
+    Pokemon.find({}, (err, pokemonItems) => {
+    res.render('Index', { pokemon : pokemonItems});
+    });
+});
 
-app.get('/pokemon/:id', function(req, res){
-    //res.send(req.params.id);
-    res.render('Show', { 
-        pokemon: pokemon[req.params.id
-    ]});
-});    
+// New - Get a form to create a new record -- WORKS
+app.get('/pokemon/new', (req, res) => {
+    res.render('New');
+});
+
+// app.get('/pokemon/new/seed', (req, res) => {
+//     res.render('New');
+// });
+
+
+// Delete - Delete this one record
+
+///***
+
+// Update - Modifying a record
+
+///***
+
+// Create - send the filled form to db and create a new record
+
+app.post('/pokemon/', (req, res) => { 
+    Pokemon.create(req.body, (err, createdPokemon) => {
+      res.redirect('/pokemon');  
+    })
+});
+
+// Edit - Get the form with the record to update
+
+///***
+
+// Show route - Show me a particular record  -- WORKS use pokemon/6393639edb35fccc0ef247ae
+
+app.get('/pokemon/:id', (req, res) => {
+    Pokemon.findById(req.params.id, (err, foundPokemon) => {
+        res.render('Show', {pokemon: foundPokemon});
+    });
+});
 
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
--- views/Index.jsx
+++ views/Index.jsx
@@ -7,25 +7,29 @@ const myStyle = {
 
 class Index extends React.Component {
   render() {
-      const { pokemon } = this.props;
+      //const { pokemon } = this.props;
       return (
               <div>
                   <h1 style={myStyle}>See All The Pokemon!</h1>
-                  <ul>
-                      {pokemon.map((pokemon, x) => {
-                          return (
-                              <li>
-                                  The{' '}
-                                  <a href={`/pokemon/${x}`}>
-                                      {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
-                                  </a>{' '}
-                                  is {pokemon.img} <br></br>
-
-                                  <br />
-                              </li>
-                          );
-                      })}
-                  </ul>
+                    <ul>
+                        {this.props.pokemon.map((pokemon, x) => {
+                            return (
+                                <li>
+                                    <a href={`/pokemon/${pokemon.id}`}>{pokemon.name.substring(0,1).toUpperCase() + pokemon.name.substring(1)}</a>
+                                </li>
+                            )
+                            {/* return (
+                                <li>
+                                    The{' '}
+                                    <a href={`/pokemon/${x}`}>
+                                        {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
+                                    </a>{' '}
+                                    is {pokemon.img} <br></br>
+                                    <br />
+                                </li>
+                            ); */}
+                        })}
+                    </ul>
               </div>
       );
   }
